2cdaaf6a18629a93db0b87d407a01b2434503d58
Vadim Komisarchik <vadim@myclok.com>
2019-08-12 09:45:47 -0400

    added behave elements

---
+++ Behave/BehaveElements.swift
@@ -0,0 +1,61 @@
+//
+//  BDElements.swift
+//  BDTestsUnitTests
+//
+//  Created by Derek Bronston on 8/2/18.
+//  Copyright © 2018 Derek Bronston. All rights reserved.
+//
+import UIKit
+
+enum  BDElementType {
+    
+    case button
+    case tabbar
+    case table
+    case collection
+    case uiSwitch
+    case barButton
+}
+
+struct BDContext {
+    let type:BDElementType
+    let element:Any
+}
+
+struct BDUIElement{
+    let type:BDElementType
+    let element:Any
+}
+
+struct BDTable {
+    let outlet:UITableView
+    let indexPath:IndexPath
+    let select:Bool
+}
+
+struct BDCollection {
+    let outlet:UICollectionView
+    let indexPath:IndexPath
+    let select:Bool
+}
+
+struct BDButton {
+    let outlet:UIButton?
+    let identifier:String?
+    let action:UIControl.Event
+    let parent:UIView?
+}
+
+struct BDTabBar {
+    let index:Int
+    let outlet:UITabBarController
+}
+
+struct BDSwitch{
+    let outlet:UISwitch
+    let action:UIControl.Event
+}
+
+struct BDBarButton{
+    let outlet:UIBarButtonItem
+}
