b56890a5e80919d2aa6efbb82e9fc6298c476c5f
Denis Efimov <denpef@gmail.com>
2020-06-22 22:00:51 +0300

    Removed core data from example

--- Example/ExampleAppTests/ExampleAppTests.swift
+++ Example/ExampleAppTests/ExampleAppTests.swift
@@ -7,21 +7,23 @@
 //
 
 import XCTest
+import Behave
 @testable import ExampleApp
 
 class ExampleAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let expectation = self.expectation(description: "\(#function)")
+        let api = Behaviour()
+        api.listenFor("master-view") {
+            expectation.fulfill()
+        }
+        
+        api.run { error in
+            if let error = error {
+                XCTFail(error)
+            }
+        }
+        waitForExpectations(timeout: 20)
     }
 
     func testPerformanceExample() throws {
