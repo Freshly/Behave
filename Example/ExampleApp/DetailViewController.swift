5b5b5d4396d468fa7eb094e37c0f351505806730
Denis Efimov <denpef@gmail.com>
2020-06-22 22:58:02 +0300

    Added test case

--- Example/ExampleApp/DetailViewController.swift
+++ Example/ExampleApp/DetailViewController.swift
@@ -24,6 +24,7 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.accessibilityIdentifier = "detail-view"
         // Do any additional setup after loading the view.
         configureView()
     }
--- Example/ExampleApp/MasterViewController.swift
+++ Example/ExampleApp/MasterViewController.swift
@@ -13,26 +13,21 @@ class MasterViewController: UITableViewController {
     var detailViewController: DetailViewController? = nil
     var objects = [Any]()
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.accessibilityIdentifier = "master-view"
+
         navigationItem.leftBarButtonItem = editButtonItem
 
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
         navigationItem.rightBarButtonItem = addButton
-        if let split = splitViewController {
-            let controllers = split.viewControllers
-            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
-        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
 
-    @objc
-    func insertNewObject(_ sender: Any) {
+    @objc func insertNewObject(_ sender: Any) {
         objects.insert(NSDate(), at: 0)
         let indexPath = IndexPath(row: 0, section: 0)
         tableView.insertRows(at: [indexPath], with: .automatic)
