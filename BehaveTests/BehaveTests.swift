f9ce702ade487d5930dbd108201cc3cd042d5784
Vadimkomis <vadim@myclok.com>
2020-07-31 18:23:33 -0400

    minor cleanup

--- BehaveTests/BehaveTests.swift
+++ BehaveTests/BehaveTests.swift
@@ -28,10 +28,4 @@ class BehaveTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-    
-    func testFake() {
-        let five = 4
-        XCTAssertEqual(five, 5)
-    }
-
 }
