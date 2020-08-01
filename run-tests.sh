981fd8cea36840492507b3f49f3bd82f7a90a5b2
Vadimkomis <vadim@myclok.com>
2020-07-31 20:43:16 -0400

    upadting  travis.yml

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
-set -o pipefail & xcodebuild test -workspace Behave.xcworkspace -scheme "Behave" COMPILER_INDEX_STORE_ENABLE=NO -destination 'platform=iOS Simulator,OS=12.2,name=iPhone iPhone Xs'| tee xcodebuild.log | xcpretty
+set -o pipefail & xcodebuild test -workspace Behave.xcworkspace -scheme "Behave" COMPILER_INDEX_STORE_ENABLE=NO -destination 'platform=iOS Simulator,OS=12.2,name=iPhone iPhone 8'| tee xcodebuild.log | xcpretty
 
 #I added the if statement because the ci was giving us a false positive on giving us a pass build, even though it did not run our tests. The logic is that if the tests do not run we then check for the status and exit the build with exit 1. The reason I put echo "BUILD FAILURE" is becasue I want it to be different then what ci is throwing when there's a legit failure, which is "BUILD FAILED"
 if [ "${PIPESTATUS[0]}" != "0" ]; then
